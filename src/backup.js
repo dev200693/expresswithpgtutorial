4823098c971d01542637e5979cb9d04edee2f982
devarshiroy93 <devarshiroy93@gmail.com>
2024-06-30 00:26:59 +0530

    basic CRUD APIs completed

---
+++ src/backup.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { Client } = require('pg');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('HELLO THERE')
+});
+
+//creating a connection pool
+const client =
+    new Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'postgres',
+        password: 'postgre',
+        port: '5432'
+    }) // How long a client is allowed to remain idle before being closed }); 
+
+//checking PG connection
+client.connect().then(() => { console.log('Connected to PostgreSQL database!'); })
+    .catch((err) => { console.error('Error connecting to the database:', err); });
+
+app.listen(3001, () => {
+
+    console.log('server started');
+});
+
+
+
+
+var obj = { "name": "Devarshi Roy", "age": 30 };
+
+
+JSON.stringify(obj);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const { Client } = require('pg');
 
+const taskRouter = express.Router();
+
 const app = express();
+
+app.use(express.json());
+
+app.use('/task', taskRouter);
 
 app.get('/', (req, res) => {
     res.send('HELLO THERE')
 });
 
-//creating a connection pool
 const client =
     new Client({
         user: 'postgres',
@@ -21,8 +26,73 @@ const client =
 client.connect().then(() => { console.log('Connected to PostgreSQL database!'); })
     .catch((err) => { console.error('Error connecting to the database:', err); });
 
+//create a task
+taskRouter.post('/', async (req, res) => {
+
+    const { description } = req.body;
+
+    const query = `insert into todos.task 
+    (description , completed) values ('${description}' , false)`
+    try {
+        await client.query(query);
+
+        return res.status(201).send({ isSuccess: true, message: 'Task creation successful' })
+    }
+    catch (err) {
+        console.log('err', err);
+        return res.status(500).send({ isSuccess: 'false', message: 'Task creation failed' });
+    }
+});
+
+//get all tasks
+//Assigment --> implement pagination
+taskRouter.get('/', async (req, res) => {
+
+
+    const query = `select * from todos.task limit 10`
+    try {
+        const response = await client.query(query);
+        const { rows } = response;
+        return res.status(200).send({ isSuccess: true, message: 'Tasks fecthing successful', data: rows })
+    }
+    catch (err) {
+        console.log('err', err);
+        return res.status(500).send({ isSuccess: 'false', message: 'Tasks fecthing failed' });
+    }
+});
+
+
+//EDIT task
+
+taskRouter.patch('/:id', async (req, res) => {
+
+    const taskId = req.params.id;
+    const description = req.body.description;
+    const query = `update todos.task set description='${description}' where id = ${taskId}`
+    try {
+        const response = await client.query(query);
+        const { rows } = response;
+        return res.status(202).send({ isSuccess: true, message: 'Task updation successful', data: rows })
+    }
+    catch (err) {
+        console.log('err', err);
+        return res.status(500).send({ isSuccess: 'false', message: 'Task updation failed' });
+    }
+});
+
+
+//creating a connection pool
+
+
 app.listen(3001, () => {
 
     console.log('server started');
 });
 
+
+
+
+var obj = { "name": "Devarshi Roy", "age": 30 };
+
+
+JSON.stringify(obj);
